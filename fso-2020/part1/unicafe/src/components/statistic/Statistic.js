8b386c1ff84ba8f5aacac3d6781f9ddb82244dbb
Kimchi Rice <kimchirice@sina.com>
2021-02-09 02:28:02 +1100

    [Mod] create seperate component for Button and Statistic

---
+++ fso-2020/part1/unicafe/src/components/statistic/Statistic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Statistic = ({name, value}) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+export default Statistic
