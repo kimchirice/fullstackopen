a89b81887a6bc5cd8b41d26827301c055b0bb9fe
kimchirice <kimchi@u12.co>
2021-02-08 20:54:27 +1100

    [Add] initial setup, all in App.js

--- fso-2020/part1/unicafe/src/App.js
+++ fso-2020/part1/unicafe/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, {useState,  } from 'react'
 import './App.css';
 
 function App() {
+
+  let initialCount = 0 
+  const [goodCount, setGoodCount] = useState(initialCount)
+  const [neutralCount, setNeutralCount] = useState(initialCount)
+  const [badCount, setBadCount] = useState(initialCount)
+  
+  const handleGoodClick = () =>{
+    let currentGoodCount = goodCount
+    setGoodCount(currentGoodCount++)
+  }
+
+  const handleBadClick = ()=>{
+    let currentBadCount = badCount
+    setBadCount(currentBadCount++)
+  }
+
+  const handleNeutralClick = () =>{
+    let currentNeutralCount = neutralCount
+    setNeutralCount(currentNeutralCount++)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div>
+        <h2>give feedback</h2>
+        <button onClick={handleGoodClick}>good</button>
+        <br></br>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <br></br>
+        <button onClick={handleBadClick}>bad</button>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <table>
+          <tr>
+            <td>&nbsp;</td>
+          <td>good</td>
+          <td>
+            {goodCount}
+          </td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>
+            {neutralCount}
+          </td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>
+            {badCount}
+          </td>
+        </tr>
+        </table>
+      </div>
     </div>
   );
 }
