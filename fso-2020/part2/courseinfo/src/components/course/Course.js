6063c36eb571da9224461092d43383914821e68b
kimchirice <kimchi@u12.co>
2021-02-10 11:22:34 +1100

    [Mod] create new components, Course.js and Header.js

---
+++ fso-2020/part2/courseinfo/src/components/course/Course.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../header/Header'
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course} /> 
+    </div>
+  )
+
+
+}
+
+export default Course
