d84ee0db2865e6fc93626c0b03e4c930225843fe
kimchirice <kimchi@u12.co>
2021-02-10 18:06:15 +1100

    [Mod] add hooks of newName and persons

--- fso-2020/part2/phonebook/src/App.js
+++ fso-2020/part2/phonebook/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App= () => {
+  const [ persons, setPersons ] = useState(
+    [
+      { name: 'Arto Hellas' }
+    ]
+  ) 
+
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log('this event is', e)
+    
+    const newNameObject = {name: newName}
+    setPersons(prev => {
+     prev.concat(newNameObject)
+    })
+    setNewName('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form >
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit} >add</button>
+        </div>
+      </form>
+
+      <div>debug: newName is {newName}</div>
+      <h2>Numbers</h2>
+      <ul>
+      
+      <li>1</li>
+        <li>2</li>
+      </ul>
+      
     </div>
-  );
+  )
 }
 
 export default App;
