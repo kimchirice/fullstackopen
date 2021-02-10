7b8a6849e8bff7dc33f45626f42b4dde41ac8872
kimchirice <kimchi@u12.co>
2021-02-10 12:54:34 +1100

    [Mod] refactor reduce in TotalExerciese

--- fso-2020/part2/courseinfo/src/components/totalExercises/TotalExercises.js
+++ fso-2020/part2/courseinfo/src/components/totalExercises/TotalExercises.js
@@ -1,7 +1,8 @@
 const TotalExercises = ({parts})=> {
-  const total = parts.reduce((sum, part) => {
-    return sum += part.exercises}
-    ,0) 
+  const total = parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  ) 
   return <h3>Total of {total} exercises</h3>
 }
 
